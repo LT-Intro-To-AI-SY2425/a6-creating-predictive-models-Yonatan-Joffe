0bb01e53a5de0786973772dad17fe21d3085827b
yjoffeturjeman@cps.edu <yjoffeturjeman@cps.edu>
2025-01-07 10:19:06 -0600

    done

--- part3-multivariable-linear-regression/a6_part3_writeup.md
+++ part3-multivariable-linear-regression/a6_part3_writeup.md
@@ -5,9 +5,9 @@ After completing `a6_part3.py` answer the following questions
 ## Questions to answer
 
 1. What is the R Squared coefficient for your model? What does that mean about the model in relation to your data?
-
+it is the accuracy of the model on a zero to one scale. my coefficent was 0.86
 2. Is your model accurate? Why or why not?
-
+yes because the R^2 is pretty accurate
 3. What does the model predict a 10-year-old car with 89000 miles is worth? What about a car that is 20 years old with 150000 miles?
-
-4. You may notice that some of your predicted results are negative. This is occurring when the value of age and the mileage of the car are very high. Why do you think this is happening?
+7957.61 -305.99
+4. You may notice that some of your predicted results are negative. This is occurring when the value of age and the mileage of the car are very high. Why do you think this is happening? because the car is so old you lost money on the investment? 
--- part4-classification/a6_part4.py
+++ part4-classification/a6_part4.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -7,21 +8,42 @@ data = pd.read_csv("part4-classification/suv_data.csv")
 data['Gender'].replace(['Male','Female'],[0,1],inplace=True)
 
 x = data[["Age", "EstimatedSalary", "Gender"]].values
-y = data["Purchased"].values
+y = data[["Purchased"]].values
 
 # Step 1: Print the values for x and y
 
 # Step 2: Standardize the data using StandardScaler, 
-
-# Step 3: Transform the data
+scaler = StandardScaler().fit(x)
+x = scaler.transform(x)
 
 # Step 4: Split the data into training and testing data
-
+x_train, x_test, y_train, y_test = train_test_split(x,y)
 # Step 5: Fit the data
-
+model = linear_model.LinearRegression().fit(x_train, y_train)
 # Step 6: Create a LogsiticRegression object and fit the data
-
+print("Accuracy:", model.score(x_test, y_train))
+print("*************")
+print("Testing Results:")
+print("")
+print(y_train)
 # Step 7: Print the score to see the accuracy of the model
+for index in range(len(x_test)):
+    x = x_test[index]
+    x = x.reshape(-1, 3)
+    print(x)
+    y_pred = int(model.predict(x))
 
+    if y_pred == 0:
+        y_pred = "male"
+    elif y_pred == 1:
+        y_pred = "female"
+    
+    actual = y_train[index]
+    if actual == 0:
+        actual = "male"
+    elif actual == 1:
+        actual = "female"
+    print("Predicted gender: " + y_pred + " Actual gender: " + actual)
+    print("")
 # Step 8: Print out the actual ytest values and predicted y values
 # based on the xtest data
